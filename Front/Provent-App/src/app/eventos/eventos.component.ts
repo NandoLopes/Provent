4f77e4418b4fe26f615a73451a8a74449ead73a6
Fernando Lopes <fernando.cruz@modalgr.com.br>
2021-10-13 09:03:14 -0300

    Basic UI, search filter, nav bar.

--- Front/Provent-App/src/app/eventos/eventos.component.ts
+++ Front/Provent-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  larguraImg: number = 80;
+  margemImg: number = 2;
+
+  isCollapsed: boolean = false;
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(
     private http: HttpClient
@@ -21,7 +46,10 @@ export class EventosComponent implements OnInit {
     public getEventos(): void{
 
       this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
       );
     }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Study project using .Net, Angular, Bootstrap, Entity Framework, and Sqlite.
 
-An Event Manager web SPA, covering the basics of a complete functional project, for portfolio.
+An Event Manager web SPA, covering the basics of a complete functional project, for portfolio.  
